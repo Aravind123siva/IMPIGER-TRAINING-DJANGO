515bd719b60a30c7ad8b9e5be87f448a780900aa
Aravindsivagurunathan <aravind.sivagurunathan@impigertech.com>
2025-02-18 17:36:32 +0530

    Blog

---
+++ Blog/Blogpost/views.py
@@ -0,0 +1,20 @@
+from rest_framework import viewsets, permissions
+from .models import BlogPost, Comment
+from .serializers import BlogPostSerializer, CommentSerializer
+
+class BlogPostViewSet(viewsets.ModelViewSet):
+    queryset = BlogPost.objects.all()
+    serializer_class = BlogPostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)

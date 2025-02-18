e0dd4b85f5a292b047628862e56a1ab5275123b0
Aravindsivagurunathan <aravind.sivagurunathan@impigertech.com>
2025-02-18 18:43:41 +0530

    filter added

--- Blog/Blogpost/views.py
+++ Blog/Blogpost/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions,filters
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import BlogPost, Comment
 from .serializers import BlogPostSerializer, CommentSerializer
 
@@ -6,6 +7,11 @@ class BlogPostViewSet(viewsets.ModelViewSet):
     queryset = BlogPost.objects.all()
     serializer_class = BlogPostSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['author', 'created_at']
+    search_fields = ['title', 'content']
+    ordering_fields = ['created_at', 'title']
+    ordering = ['-created_at']  # Default ordering
 
 
 
